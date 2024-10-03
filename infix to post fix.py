4ca12f2c39318454ad5db80257e71e275777645d
RAJASHREE P <167889892+Rajashreepari@users.noreply.github.com>
2024-10-03 14:30:30 +0530

    python practice code

---
+++ infix to post fix.py
@@ -0,0 +1,31 @@
+def prec(c):
+    if c=="^":
+        return 2
+    elif c=="/" or c=="*":
+        return 1
+    elif c=="+" or c=="-":
+        return 0
+    else:
+        return-1
+def infixtopostfix(s):
+    postfix=[]
+    stack=[]
+    for i in  range(len(s)):
+        c=s[i]
+        if("a"<=c<="z") or ("A"<=c<="Z") or ("0"<=c<="9"):
+            postfix.append(c)
+        elif c=="(":
+            stack.append(c)
+        elif c==")":
+            while stack and stack[-1]!="(":
+                postfix.append(stack.pop())
+            stack.pop()
+        else:
+            while stack and (prec(s[i])<prec(stack[-1]) or prec(s[i])==prec(stack[-1])):
+                 postfix.append(stack.pop())
+            stack.append(s[i])
+    while stack:
+        postfix.append(stack.pop())
+    print("".join(postfix))
+s=input()
+infixtopostfix(s)
